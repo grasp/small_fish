ab2a26e9b8af5913ad13774f13fbe2ea1c8c6458
hunter <hunter.wxhu@gmail.com>
2013-10-23 07:50:04 +0800

    update on 2013-10-23 - 7:50:04.82

--- lib/signal/buy_signal.rb
+++ lib/signal/buy_signal.rb
@@ -8,8 +8,27 @@ source File.expand_path("../buy_policy_1.yml",__FILE__)
 
 end
 
-def generate_price_buy_signal(price_array,buy_policy_class)
-	puts (buy_policy_class.price.price_open_bigger_close) && (price_array[0].to_f > price_array[3].to_f)
+#bpc :buy policy class
+#依次为开盘，最高，最低，收盘，成交量
+def generate_price_buy_signal(price_array,bpc)
+	buy_signal=true
+
+	#开盘价大于收盘价
+	unless bpc.price.nil?
+	 buy_signal &&=bpc.price.open_bigger_close  && (price_array[0].to_f > price_array[3].to_f) if  bpc.price.respond_to?("open_bigger_close")
+    #开盘价为最高价
+	 buy_signal &&=bpc.price.open_equal_high  && (price_array[0].to_f == price_array[1].to_f) if  bpc.price.respond_to?("open_equal_high")
+	#开盘价为最低价
+	 buy_signal &&=bpc.price.open_equal_low  && (price_array[0].to_f == price_array[2].to_f) if   bpc.price.respond_to?("open_equal_low")
+    #收盘价为最低价
+    buy_signal &&=bpc.price.close_equal_low  && (price_array[3].to_f == price_array[2].to_f) if  bpc.price.respond_to?("close_equal_low")
+    #收盘价为最高价
+    buy_signal &&=bpc.price.close_equal_high  && (price_array[3].to_f == price_array[1].to_f) if  bpc.price.respond_to?("close_equal_high ")
+    
+    puts buy_signal
+    end
+
+
 end
 
 def generate_macd_buy_signal(macd_array,buy_policy_class)
