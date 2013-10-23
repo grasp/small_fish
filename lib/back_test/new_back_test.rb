e85bf88a9833709f1811d5f3c9c63f0ca60e3b54
hunter <hunter.wxhu@gmail.com>
2013-10-24 07:57:43 +0800

    update on 2013-10-24 - 7:57:43.00

--- lib/back_test/new_back_test.rb
+++ lib/back_test/new_back_test.rb
@@ -4,7 +4,7 @@ require File.expand_path("../../signal/buy_signal.rb",__FILE__)
 
 def back_test_one_stock(symbol,buy_policy_file,sell_policy_file,start_index,end_index)
     buy_policy_class=BuySettings.new(buy_policy_file)
-    sell_policy_class=BuySettings.new(sell_policy_file)
+    sell_policy_class=SellSettings.new(sell_policy_file)
 
     processed_data=read_data_process_file(symbol)
 
@@ -20,15 +20,57 @@ def back_test_one_stock(symbol,buy_policy_file,sell_policy_file,start_index,end_
 
     #puts volume_hash
 
-    price_array=price_hash.to_a
-    price_array[start_index,end_index].each do |price|
-    	date=price[0]
-    	price_array=price[1]
+     full_price_array=price_hash.to_a
+
+    #price_array[start_index,end_index].reverse.each_index do |index|
+    (end_index-1).downto(start_index).each do |index|
+    	date=full_price_array[index][0]
+        #puts "first date=#{date},index=#{index}"
+    	price_array=full_price_array[index][1]
+
     	macd_array=macd_hash[date]
-    	low_price_array=low_price_hash[date]
+        low_price_array=low_price_hash[date]
     	high_price_array=high_price_hash[date]
     	volume_array=volume_hash[date]
-        puts generate_price_buy_signal(price_array,buy_policy_class)
+        #puts "last price =#{price_array[27]}"
+        last_date=full_price_array[index-1][0]
+        last_price=full_price_array[index-1][1]
+        last_macd_array=macd_hash[last_date]
+        last_low_price_array=low_price_hash[last_date]
+        last_high_price_array=high_price_hash[last_date]
+        last_volume_array=volume_hash[last_date]
+
+        buy_signal=true
+        sell_signal=true
+
+        price_signal = generate_price_buy_signal(price_array,buy_policy_class)
+        macd_signal = generate_macd_buy_signal(macd_array,buy_policy_class)
+
+        last_macd_signal = generate_last_macd_buy_signal(last_macd_array,buy_policy_class)
+
+        low_price_signal = generate_low_price_buy_signal(low_price_array,price_array,buy_policy_class)
+        high_price_signal = generate_high_price_buy_signal(high_price_array,price_array,buy_policy_class)
+        volume_signal = generate_volume_buy_signal(volume_array,buy_policy_class)
+
+        #puts "price_signal=#{price_signal},macd_signal=#{macd_signal},last_macd_signal=#{last_macd_signal},low_price_signal=#{low_price_signal},high_price_signal=#{high_price_signal},volume_signal=#{volume_signal}"
+        buy_signal=price_signal && macd_signal && last_macd_signal && low_price_signal && high_price_signal && volume_signal
+        if buy_signal
+        puts "buy on #{date} with price #{price_array[3]}" 
+        print last_macd_array
+        puts ""
+         print  macd_array
+
+        end
+
+        #BuySettings.reload!
+        sell_signal &&= generate_price_buy_signal(price_array,sell_policy_class)
+        sell_signal &&= generate_macd_buy_signal(macd_array,sell_policy_class)
+        sell_signal &&= generate_last_macd_buy_signal(last_macd_array,sell_policy_class)
+
+        sell_signal &&= generate_low_price_buy_signal(low_price_array,price_array,sell_policy_class)
+        sell_signal &&= generate_high_price_buy_signal(high_price_array,price_array,sell_policy_class)
+        sell_signal &&= generate_volume_buy_signal(volume_array,sell_policy_class)
+        puts "sell on #{date} with price #{price_array[3]}"  if sell_signal
 
     end
 
@@ -41,7 +83,7 @@ if $0==__FILE__
 	sell_policy_file=File.expand_path("../../signal/sell_policy_1.yml",__FILE__)
     symbol="000009.sz"
 
-    back_test_one_stock(symbol,buy_policy_file,sell_policy_file,10,30)
+    back_test_one_stock(symbol,buy_policy_file,sell_policy_file,10,50)
 
 
 end
--- lib/signal/buy_signal.rb
+++ lib/signal/buy_signal.rb
@@ -8,43 +8,130 @@ source File.expand_path("../buy_policy_1.yml",__FILE__)
 
 end
 
+class SellSettings < Settingslogic
+source File.expand_path("../sell_policy_1.yml",__FILE__)
+ # source File.expand_path("../buy_policy_1.yml",__FILE__)
+end
+
 #bpc :buy policy class
 #依次为开盘，最高，最低，收盘，成交量
 def generate_price_buy_signal(price_array,bpc)
 	buy_signal=true
 
 	#开盘价大于收盘价
-	unless bpc.price.nil?
-	 buy_signal &&=bpc.price.open_bigger_close  && (price_array[0].to_f > price_array[3].to_f) if  bpc.price.respond_to?("open_bigger_close")
-    #开盘价为最高价
-	 buy_signal &&=bpc.price.open_equal_high  && (price_array[0].to_f == price_array[1].to_f) if  bpc.price.respond_to?("open_equal_high")
-	#开盘价为最低价
-	 buy_signal &&=bpc.price.open_equal_low  && (price_array[0].to_f == price_array[2].to_f) if   bpc.price.respond_to?("open_equal_low")
-    #收盘价为最低价
-    buy_signal &&=bpc.price.close_equal_low  && (price_array[3].to_f == price_array[2].to_f) if  bpc.price.respond_to?("close_equal_low")
-    #收盘价为最高价
-    buy_signal &&=bpc.price.close_equal_high  && (price_array[3].to_f == price_array[1].to_f) if  bpc.price.respond_to?("close_equal_high ")
+	if bpc.respond_to?("price")
+	 buy_signal &&=bpc.price.open_bigger_close  == (price_array[0].to_f > price_array[3].to_f) if  bpc.price.respond_to?("open_bigger_close")
+     #开盘价为最高价
+	 buy_signal &&=bpc.price.open_equal_high  == (price_array[0].to_f == price_array[1].to_f) if  bpc.price.respond_to?("open_equal_high")
+	 #开盘价为最低价
+	 buy_signal &&=bpc.price.open_equal_low  == (price_array[0].to_f == price_array[2].to_f) if   bpc.price.respond_to?("open_equal_low")
+     #收盘价为最低价
+     buy_signal &&=bpc.price.close_equal_low  == (price_array[3].to_f == price_array[2].to_f) if  bpc.price.respond_to?("close_equal_low")
+     #收盘价为最高价
+     buy_signal &&=bpc.price.close_equal_high  == (price_array[3].to_f == price_array[1].to_f) if  bpc.price.respond_to?("close_equal_high ")
     
-    puts buy_signal
+     end
+      buy_signal
+end
+
+#MACD 1,2,3,4,5,10,20,30,60,100,120,200
+def generate_macd_buy_signal(macd_array,bpc)
+	buy_signal=true
+	if bpc.respond_to?("macd")
+	 buy_signal &&=bpc.macd.macd2_bigger_macd5  == (macd_array[1].to_f > macd_array[4].to_f) if  bpc.macd.respond_to?("macd2_bigger_macd5")
+  	 buy_signal &&=bpc.macd.macd5_bigger_macd10  == (macd_array[4].to_f > macd_array[5].to_f) if  bpc.macd.respond_to?("macd5_bigger_macd10")
+     buy_signal &&=bpc.macd.macd5_bigger_macd20  == (macd_array[4].to_f > macd_array[6].to_f) if  bpc.macd.respond_to?("macd5_bigger_macd20")
+     buy_signal &&=bpc.macd.macd5_bigger_macd30  == (macd_array[4].to_f > macd_array[7].to_f) if  bpc.macd.respond_to?("macd5_bigger_macd30")
+     buy_signal &&=bpc.macd.macd5_bigger_macd60  == (macd_array[4].to_f > macd_array[8].to_f) if  bpc.macd.respond_to?("macd5_bigger_macd60")
+     buy_signal &&=bpc.macd.macd5_bigger_macd100  == (macd_array[4].to_f > macd_array[9].to_f) if  bpc.macd.respond_to?("macd5_bigger_macd100")
+     buy_signal &&=bpc.macd.macd5_bigger_macd120  == (macd_array[4].to_f > macd_array[10].to_f) if  bpc.macd.respond_to?("macd5_bigger_macd120")
+     buy_signal &&=bpc.macd.macd5_bigger_macd200  == (macd_array[4].to_f > macd_array[11].to_f) if  bpc.macd.respond_to?("macd5_bigger_macd200")
+     buy_signal &&=bpc.macd.macd10_bigger_macd20  == (macd_array[5].to_f > macd_array[6].to_f) if  bpc.macd.respond_to?("macd10_bigger_macd20")
+     buy_signal &&=bpc.macd.macd10_bigger_macd30  == (macd_array[5].to_f > macd_array[7].to_f) if  bpc.macd.respond_to?("macd10_bigger_macd30")
+     buy_signal &&=bpc.macd.macd10_bigger_macd60  == (macd_array[5].to_f > macd_array[8].to_f) if  bpc.macd.respond_to?("macd10_bigger_macd60")
+     buy_signal &&=bpc.macd.macd10_bigger_macd100  == (macd_array[5].to_f > macd_array[9].to_f) if  bpc.macd.respond_to?("macd10_bigger_macd100")
+     buy_signal &&=bpc.macd.macd10_bigger_macd120  == (macd_array[5].to_f > macd_array[10].to_f) if  bpc.macd.respond_to?("macd10_bigger_macd120")
+     buy_signal &&=bpc.macd.macd10_bigger_macd200 == (macd_array[5].to_f > macd_array[11].to_f) if  bpc.macd.respond_to?("macd10_bigger_macd200")
+     buy_signal &&=bpc.macd.macd20_bigger_macd30  == (macd_array[6].to_f > macd_array[7].to_f) if  bpc.macd.respond_to?("macd20_bigger_macd30")
+     buy_signal &&=bpc.macd.macd20_bigger_macd60  == (macd_array[6].to_f > macd_array[8].to_f) if  bpc.macd.respond_to?("macd20_bigger_macd60")
+     buy_signal &&=bpc.macd.macd20_bigger_macd100  == (macd_array[6].to_f > macd_array[9].to_f) if  bpc.macd.respond_to?("macd20_bigger_macd100")
+     buy_signal &&=bpc.macd.macd20_bigger_macd120  == (macd_array[6].to_f > macd_array[10].to_f) if  bpc.macd.respond_to?("macd20_bigger_macd120")
+     buy_signal &&=bpc.macd.macd20_bigger_macd200  == (macd_array[6].to_f > macd_array[11].to_f) if  bpc.macd.respond_to?("macd20_bigger_macd200")    
     end
-
-
+  buy_signal
 end
 
-def generate_macd_buy_signal(macd_array,buy_policy_class)
-	puts (buy_policy_class.price.price_open_bigger_close) && (price_array[0].to_f > price_array[3].to_f)
+#MACD 1,2,3,4,5,10,20,30,60,100,120,200
+def generate_last_macd_buy_signal(macd_array,bpc)
+	buy_signal=true
+	if bpc.respond_to?("last_macd")
+	 buy_signal &&=bpc.last_macd.macd2_bigger_macd5  == (macd_array[1].to_f > macd_array[4].to_f) if  bpc.last_macd.respond_to?("macd2_bigger_macd5")
+  	 buy_signal &&=bpc.last_macd.macd5_bigger_macd10  == (macd_array[4].to_f > macd_array[5].to_f) if  bpc.last_macd.respond_to?("macd5_bigger_macd10")
+     buy_signal &&=bpc.last_macd.macd5_bigger_macd20  == (macd_array[4].to_f > macd_array[6].to_f) if  bpc.last_macd.respond_to?("macd5_bigger_macd20")
+     buy_signal &&=bpc.last_macd.macd5_bigger_macd30  == (macd_array[4].to_f > macd_array[7].to_f) if  bpc.last_macd.respond_to?("macd5_bigger_macd30")
+     buy_signal &&=bpc.last_macd.macd5_bigger_macd60  == (macd_array[4].to_f > macd_array[8].to_f) if  bpc.last_macd.respond_to?("macd5_bigger_macd60")
+     buy_signal &&=bpc.last_macd.macd5_bigger_macd100  == (macd_array[4].to_f > macd_array[9].to_f) if  bpc.last_macd.respond_to?("macd5_bigger_macd100")
+     buy_signal &&=bpc.last_macd.macd5_bigger_macd120  == (macd_array[4].to_f > macd_array[10].to_f) if  bpc.last_macd.respond_to?("macd5_bigger_macd120")
+     buy_signal &&=bpc.last_macd.macd5_bigger_macd200  == (macd_array[4].to_f > macd_array[11].to_f) if  bpc.last_macd.respond_to?("macd5_bigger_macd200")
+     buy_signal &&=bpc.last_macd.macd10_bigger_macd20  == (macd_array[5].to_f > macd_array[6].to_f) if  bpc.last_macd.respond_to?("macd10_bigger_macd20")
+     buy_signal &&=bpc.last_macd.macd10_bigger_macd30  == (macd_array[5].to_f > macd_array[7].to_f) if  bpc.last_macd.respond_to?("macd10_bigger_macd30")
+     buy_signal &&=bpc.last_macd.macd10_bigger_macd60  == (macd_array[5].to_f > macd_array[8].to_f) if  bpc.last_macd.respond_to?("macd10_bigger_macd60")
+     buy_signal &&=bpc.last_macd.macd10_bigger_macd100  == (macd_array[5].to_f > macd_array[9].to_f) if  bpc.last_macd.respond_to?("macd10_bigger_macd100")
+     buy_signal &&=bpc.last_macd.macd10_bigger_macd120  == (macd_array[5].to_f > macd_array[10].to_f) if  bpc.last_macd.respond_to?("macd10_bigger_macd120")
+     buy_signal &&=bpc.last_macd.macd10_bigger_macd200 == (macd_array[5].to_f > macd_array[11].to_f) if  bpc.last_macd.respond_to?("macd10_bigger_macd200")
+     buy_signal &&=bpc.last_macd.macd20_bigger_macd30  == (macd_array[6].to_f > macd_array[7].to_f) if  bpc.last_macd.respond_to?("macd20_bigger_macd30")
+     buy_signal &&=bpc.last_macd.macd20_bigger_macd60  == (macd_array[6].to_f > macd_array[8].to_f) if  bpc.last_macd.respond_to?("macd20_bigger_macd60")
+     buy_signal &&=bpc.last_macd.macd20_bigger_macd100  == (macd_array[6].to_f > macd_array[9].to_f) if  bpc.last_macd.respond_to?("macd20_bigger_macd100")
+     buy_signal &&=bpc.last_macd.macd20_bigger_macd120  == (macd_array[6].to_f > macd_array[10].to_f) if  bpc.last_macd.respond_to?("macd20_bigger_macd120")
+     buy_signal &&=bpc.last_macd.macd20_bigger_macd200  == (macd_array[6].to_f > macd_array[11].to_f) if  bpc.last_macd.respond_to?("macd20_bigger_macd200")    
+    end
+   # puts "last_buy_signal=#{buy_signal}"
+  buy_signal
 end
 
-def generate_low_price_buy_signal(low_price_array,buy_policy_class)
-	puts (buy_policy_class.price.price_open_bigger_close) && (price_array[0].to_f > price_array[3].to_f)
+#  low price array [1,2,3,4,5,10,20,30,60,100,120]
+def generate_low_price_buy_signal(low_price_array,price_array,bpc)
+	buy_signal=true
+	if bpc.respond_to?("low_price")
+	  buy_signal &&=bpc.low_price.lowest_5_day  == (low_price_array[4].to_f < price_array[3].to_f) if  bpc.low_price.respond_to?("lowest_5_day")
+      buy_signal &&=bpc.low_price.lowest_10_day  == (low_price_array[5].to_f < price_array[3].to_f) if  bpc.low_price.respond_to?("lowest_10_day")
+      buy_signal &&=bpc.low_price.lowest_20_day  == (low_price_array[6].to_f < price_array[3].to_f) if  bpc.low_price.respond_to?("lowest_20_day")
+      buy_signal &&=bpc.low_price.lowest_30_day  == (low_price_array[7].to_f < price_array[3].to_f) if  bpc.low_price.respond_to?("lowest_30_day")
+      buy_signal &&=bpc.low_price.lowest_60_day  == (low_price_array[8].to_f < price_array[3].to_f) if  bpc.low_price.respond_to?("lowest_60_day")
+      buy_signal &&=bpc.low_price.lowest_100_day  == (low_price_array[9].to_f < price_array[3].to_f) if  bpc.low_price.respond_to?("lowest_100_day")
+      buy_signal &&=bpc.low_price.lowest_120__day  == (low_price_array[10].to_f < price_array[3].to_f) if  bpc.low_price.respond_to?("lowest_120_day")
+    end
+    buy_signal
 end
 
-def generate_high_price_buy_signal(high_price_array,buy_policy_class)
-	puts (buy_policy_class.price.price_open_bigger_close) && (price_array[0].to_f > price_array[3].to_f)
+#  high price array [1,2,3,4,5,10,20,30,60,100,120]
+def generate_high_price_buy_signal(high_price_array,price_array,bpc)
+    buy_signal=true
+	if bpc.respond_to?("high_price")
+	  buy_signal &&=bpc.high_price.highest_5_day  == (high_price_array[4].to_f < price_array[3].to_f) if  bpc.high_price.respond_to?("highest_5_day")
+      buy_signal &&=bpc.high_price.highest_10_day  == (high_price_array[5].to_f < price_array[3].to_f) if  bpc.high_price.respond_to?("highest_10_day")
+      buy_signal &&=bpc.high_price.highest_20_day  == (high_price_array[6].to_f < price_array[3].to_f) if  bpc.high_price.respond_to?("highest_20_day")
+      buy_signal &&=bpc.high_price.highest_30_day  == (high_price_array[7].to_f < price_array[3].to_f) if  bpc.high_price.respond_to?("highest_30_day")
+      buy_signal &&=bpc.high_price.highest_60_day  == (high_price_array[8].to_f < price_array[3].to_f) if  bpc.high_price.respond_to?("highest_60_day")
+      buy_signal &&=bpc.high_price.highest_100_day  == (high_price_array[9].to_f < price_array[3].to_f) if  bpc.high_price.respond_to?("highest_100_day")
+      buy_signal &&=bpc.high_price.highest_120__day  == (high_price_array[10].to_f < price_array[3].to_f) if  bpc.high_price.respond_to?("highest_120_day")
+    end
+    buy_signal
 end
 
-def generate_volume_buy_signal(volume_array,buy_policy_class)
-	puts (buy_policy_class.price.price_open_bigger_close) && (price_array[0].to_f > price_array[3].to_f)
+#   [1,2,3,4,5,10,20,30,60,100,120]
+def generate_volume_buy_signal(volume_array,bpc)
+
+	 buy_signal=true
+	 if bpc.respond_to?("volume")
+	   buy_signal &&=bpc.volume.volume2_bigger_volume5  == (volume_array[1].to_f > volume_array[4].to_f) if  bpc.volume.respond_to?("volume2_bigger_volume5")
+  	   buy_signal &&=bpc.volume.volume5_bigger_volume10  == (volume_array[4].to_f > volume_array[5].to_f) if  bpc.volume.respond_to?("volume5_bigger_volume10")
+       buy_signal &&=bpc.volume.volume5_bigger_volume20  == (volume_array[4].to_f > volume_array[6].to_f) if  bpc.volume.respond_to?("volume5_bigger_volume20")
+       buy_signal &&=bpc.volume.volume5_bigger_volume30  == (volume_array[4].to_f > volume_array[7].to_f) if  bpc.volume.respond_to?("volume5_bigger_volume30")
+       buy_signal &&=bpc.volume.volume5_bigger_volume60  == (volume_array[4].to_f > volume_array[8].to_f) if  bpc.volume.respond_to?("volume5_bigger_volume60")
+       buy_signal &&=bpc.volume.volume5_bigger_volume100  == (volume_array[4].to_f > volume_array[9].to_f) if  bpc.volume.respond_to?("volume5_bigger_volume100")
+     end
+     buy_signal
 end
 
 
